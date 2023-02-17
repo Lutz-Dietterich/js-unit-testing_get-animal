02305984edf984553491b38ff87cddda249dda92
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-02-17 12:28:38 +0100

    finishChallenge

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-export function getAnimal() {}
+export function getAnimal(currywurst) {
+  let result;
+  if (currywurst === "cats") {
+    result = `I totally love ${currywurst}!`;
+  } else if (currywurst === "dogs") {
+    result = `I like ${currywurst}!`;
+  } else {
+    result = `I do not like animals at all!`;
+  }
+  return result;
+}
+
+// function testNum(a) {
+//     let result;
+//     if (a > 0) {
+//       result = 'positive';
+//     } else {
+//       result = 'NOT positive';
+//     }
+//     return result;
+//   }
